9de9082e85bc3621c492ccd8c75f7bc0036879e5
Robert Goldmann <deadlocker@gmx.de>
2021-01-12 23:58:53 +0100

    set summary for device routes

--- src/routers/DeviceRouter.py
+++ src/routers/DeviceRouter.py
@@ -7,18 +7,21 @@ from Dependencies import get_database
 from logic.databaseNew import Schemas, Crud
 
 router = APIRouter(
-    prefix='/devices',
-    tags=['devices'],
+    prefix='/device',
+    tags=['device'],
     responses={404: {'description': 'Not found'}},
 )
 
 
-@router.get('/', response_model=List[Schemas.Device])
+@router.get('/', response_model=List[Schemas.Device],
+            summary='Gets all devices')
 async def read_devices(skip: int = 0, limit: int = 100, db: Session = Depends(get_database)):
     return Crud.get_devices(db, skip=skip, limit=limit)
 
 
-@router.get('/{deviceId}', response_model=Schemas.Device, responses={404: {'description': 'Device not found'}})
+@router.get('/{deviceId}', response_model=Schemas.Device,
+            summary='Gets a specific device',
+            responses={404: {'description': 'Device not found'}})
 async def read_device(deviceId: int, db: Session = Depends(get_database)):
     device = Crud.get_device(db, deviceId=deviceId)
     if device is None:
@@ -26,7 +29,9 @@ async def read_device(deviceId: int, db: Session = Depends(get_database)):
     return device
 
 
-@router.post('/', response_model=Schemas.Device, responses={400: {'description': 'Device with this name already exists'}})
+@router.post('/', response_model=Schemas.Device,
+             summary='Adds a new device',
+             responses={400: {'description': 'Device with this name already exists'}})
 async def create_user(device: Schemas.DeviceCreate, db: Session = Depends(get_database)):
     createdDevice = Crud.get_device_by_name(db, device.name)
     if createdDevice:
